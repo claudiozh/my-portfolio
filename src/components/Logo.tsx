330191c02cfea2aee796f546c0e0281b19362fd6
claudiozh <claudiorodrigozh@gmail.com>
2023-05-22 14:15:53 -0300

    fix: add max height in logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,5 +2,13 @@ import { pathImages } from '@/utils/path-images'
 import Image from 'next/image'
 
 export const Logo = () => {
-  return <Image src={pathImages.logo} alt="Logo" width={140} height={80} />
+  return (
+    <Image
+      src={pathImages.logo}
+      alt="Logo"
+      width={140}
+      height={80}
+      className="max-h-11"
+    />
+  )
 }
